1a2bfacc12259b0690540fa5fadc926955718780
srirampotla <42873234+POTLASRIRAM@users.noreply.github.com>
2022-03-28 23:07:54 +0530

    Add files via upload

---
+++ Sumthree.py
@@ -0,0 +1,7 @@
+n1=int(input("Enter first number "))
+n2=int(input("Enter second number "))
+n3=int(input("Enter third number "))
+if(n1==n2 or n2==n3 or n1==n3):
+    print("Zero")
+else:
+    print(n1+n2+n3)
